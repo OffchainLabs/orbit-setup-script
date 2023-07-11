8cca26c60398bafcd90765a35645e7253510d254
GreatSoshiant <salehi.mehdi70@gmail.com>
2023-07-11 10:09:27 -0700

    exit the das-server on rollup mode

--- das-server.sh
+++ das-server.sh
@@ -8,6 +8,7 @@ if jq -e '.node | has("data-availability")' /home/user/.arbitrum/nodeConfig.json
     # Start daserver with the new command
     /usr/local/bin/daserver --data-availability.parent-chain-node-url https://goerli-rollup.arbitrum.io/rpc --enable-rpc --rpc-addr '0.0.0.0' --enable-rest --rest-addr '0.0.0.0' --log-level 3 --data-availability.local-db-storage.enable --data-availability.local-db-storage.data-dir /home/user/data/badgerdb --data-availability.local-db-storage.discard-after-timeout --data-availability.key.key-dir /home/user/.arbitrum/keys --data-availability.local-cache.enable --data-availability.sequencer-inbox-address $SEQUENCER_INBOX_ADDRESS
 else
-    # No-op (do nothing) if "data-availability" key doesn't exist
-    sleep infinity
+    # Log a message and exit if "data-availability" key doesn't exist
+    echo "You're running in Rollup mode. No need for das-server, so exiting the container ..."
+    exit
 fi
