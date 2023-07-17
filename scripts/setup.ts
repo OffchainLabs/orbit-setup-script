e2de46596fe748707f7a184c2032230789f48ae7
Jason-Wanxt <wanxutao123@gmail.com>
2023-07-17 13:44:13 +0800

    add resume functionality

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -5,6 +5,7 @@ import fs from 'fs';
 import { ethDeposit } from './ethDeposit'; 
 import {l3Configuration } from './l3Configuration';
 import {tokenBridgeDeployment} from './tokenBridgeDeployment'
+import { defaultRunTimeState, RuntimeState } from "./runTimeState";
 
 // Delay function
 function delay(ms: number) {
@@ -24,6 +25,13 @@ async function main() {
     // Read the JSON configuration
     const configRaw = fs.readFileSync('./config/orbitSetupScriptConfig.json', 'utf-8');
     const config: L3Config = JSON.parse(configRaw);
+    let rs: RuntimeState
+    if(fs.existsSync('./config/resumeState.json')) {
+        const stateRaw = fs.readFileSync('./config/resumeState.json', 'utf-8');
+        rs = JSON.parse(stateRaw);
+    } else {
+        rs = defaultRunTimeState
+    }
 
     // Generating providers from RPCs
     const L2Provider = new ethers.providers.JsonRpcProvider(L2_RPC_URL);
@@ -37,54 +45,63 @@ async function main() {
     // Creating the signer
     const signer = new ethers.Wallet(privateKey).connect(L2Provider);
     
-    ////////////////////////////////////////////////
-    /// Funding batch-poster and staker address ///
-    //////////////////////////////////////////////
-    console.log("Funding batch-poster and staker accounts on Arbitrum Goerli each with 0.3 ETH")
-    const tx1 = await signer.sendTransaction({
-        to: config.batchPoster,
-        value: ethers.utils.parseEther("0.3")
-    });
-
-    console.log(`Transaction hash on Arbitrum Goerli: ${tx1.hash}`);
-    const receipt1 = await tx1.wait();
-    console.log(`Transaction was mined in block ${receipt1.blockNumber} on Arbitrum Goerli`);
-
-    const tx2 = await signer.sendTransaction({
-        to: config.staker,
-        value: ethers.utils.parseEther("0.3")
-    });
-
-    console.log(`Transaction hash on Arbitrum Goerli: ${tx2.hash}`);
-    const receipt2 = await tx2.wait();
-    console.log(`Transaction was mined in block ${receipt2.blockNumber} on Arbitrum Goerli`);
-
-
     try {
-        ////////////////////////////////
-        /// ETH deposit to L3 /////////
-        //////////////////////////////        
-        console.log("Running ethDeposit Script to Deposit ETH from Arbitrum Goerli to your account on appchain ... 💰💰💰💰💰💰");
-        let oldBalance = await L3Provider.getBalance(config.chainOwner);
-        await ethDeposit(privateKey, L2_RPC_URL, L3_RPC_URL);
+        ////////////////////////////////////////////////
+        /// Funding batch-poster and staker address ///
+        //////////////////////////////////////////////
+        if(!rs.etherSent.batchPoster) {
+            console.log("Funding batch-poster accounts on Arbitrum Goerli  with 0.3 ETH")
+            const tx1 = await signer.sendTransaction({
+                to: config.batchPoster,
+                value: ethers.utils.parseEther("0.3")
+            });
+            console.log(`Transaction hash on Arbitrum Goerli: ${tx1.hash}`);
+            const receipt1 = await tx1.wait();
+            console.log(`Transaction was mined in block ${receipt1.blockNumber} on Arbitrum Goerli`);
+            rs.etherSent.batchPoster = true
+        }
+        
+        if(!rs.etherSent.staker) {
+            console.log("Funding staker accounts on Arbitrum Goerli with 0.3 ETH")
+            const tx2 = await signer.sendTransaction({
+                to: config.staker,
+                value: ethers.utils.parseEther("0.3")
+            });
+            console.log(`Transaction hash on Arbitrum Goerli: ${tx2.hash}`);
+            const receipt2 = await tx2.wait();
+            console.log(`Transaction was mined in block ${receipt2.blockNumber} on Arbitrum Goerli`);
+            rs.etherSent.staker = true
+        }
+        
 
-        // Waiting for 30 secs to be sure that ETH deposited is received on L3
-        // Repeatedly check the balance until it changes by 1 Ether
-        while (true) {
-            let newBalance = await L3Provider.getBalance(config.chainOwner);
-            if (newBalance.sub(oldBalance).gte(ethers.utils.parseEther("0.4"))) {
-                console.log("Balance of your account on appchain increased by 0.4 Ether.");
-                break;
+        
+        if(!rs.etherSent.deposit) {
+            ////////////////////////////////
+            /// ETH deposit to L3 /////////
+            //////////////////////////////        
+            console.log("Running ethDeposit Script to Deposit ETH from Arbitrum Goerli to your account on appchain ... 💰💰💰💰💰💰");
+            let oldBalance = await L3Provider.getBalance(config.chainOwner);
+            await ethDeposit(privateKey, L2_RPC_URL, L3_RPC_URL);
+
+            // Waiting for 30 secs to be sure that ETH deposited is received on L3
+            // Repeatedly check the balance until it changes by 1 Ether
+            while (true) {
+                let newBalance = await L3Provider.getBalance(config.chainOwner);
+                if (newBalance.sub(oldBalance).gte(ethers.utils.parseEther("0.4"))) {
+                    console.log("Balance of your account on appchain increased by 0.4 Ether.");
+                    break;
+                }
+                console.log("Balance not changed yet. Waiting for another 30 seconds ⏰⏰⏰⏰⏰⏰");
+                await delay(30 * 1000);
             }
-            console.log("Balance not changed yet. Waiting for another 30 seconds ⏰⏰⏰⏰⏰⏰");
-            await delay(30 * 1000);
+            rs.etherSent.deposit = true
         }
 
         ////////////////////////////////
         /// Token Bridge Deployment ///
         //////////////////////////////
         console.log("Running tokenBridgeDeployment script to deploy token bridge contracts on Arbitrum Goerli and your appchain 🌉🌉🌉🌉🌉");
-        await tokenBridgeDeployment(privateKey, L2_RPC_URL, L3_RPC_URL);
+        await tokenBridgeDeployment(privateKey, L2_RPC_URL, L3_RPC_URL, rs);
     
         ////////////////////////////////
         /// L3 Chain Configuration ///
@@ -93,6 +110,9 @@ async function main() {
         await l3Configuration(privateKey, L2_RPC_URL, L3_RPC_URL);
     } catch (error) {
         console.error('Error occurred:', error);
+        const runtimeString = JSON.stringify(rs)
+        fs.writeFileSync('./config/resumeState.json', runtimeString)
+        console.log("This running record has been saved to ./config/resumeState.json , please check the cause of the error and the program will read the file and continue to execute the unfinished part. (In most failure case is out of gas)")
     }
 }
 
