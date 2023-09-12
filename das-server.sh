edf356f78b11126a24ef44c45483d296019fcc46
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2023-09-12 20:04:19 +0300

    jq issue resolved

--- das-server.sh
+++ das-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Read sequencerInbox address from the JSON file
+# Read sequencerInbox address from the orbitSetupScriptConfig JSON file
 SEQUENCER_INBOX_ADDRESS=$(jq -r '.sequencerInbox' /home/user/.arbitrum/orbitSetupScriptConfig.json)
-PARENT_CHAIN_NODE_URL=$(jq -r '.parent-chain-node-url' /home/user/.arbitrum/nodeConfig.json)
+
+# Read the parent-chain-node-url from the nodeConfig JSON file
+PARENT_CHAIN_NODE_URL=$(jq -r '.node."data-availability"."parent-chain-node-url"' /home/user/.arbitrum/nodeConfig.json)
 
 # Check if the "data-availability" key exists
 if jq -e '.node | has("data-availability")' /home/user/.arbitrum/nodeConfig.json >/dev/null; then
